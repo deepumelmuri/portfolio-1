47274a069c35b71db1e3ee6b5df0fe0bd7d4c4c9
deepumelmuri <deepumelmuri@gmail.com>
2023-11-03 10:43:55 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+function sendMail(){ console.log("hellow");
+    let params = {
+       
+        name :document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message:document.getElementById('message').value, 
+        subject:document.getElementById("subject").value,
+    };
+
+const serviceID = "service_fwi3v5b";
+const templateID = "template_15jde9f";
+emailjs.send(serviceID,templateID,params)
+.then(
+    res =>{
+        document.getElementById("name").value ="";
+        document.getElementById("email").value ="";
+        document.getElementById("message").value ="";
+        document.getElementById("subject").value ="";
+        console.log(res);
+        alert("your message send is sucessfully");
+    })
+    .catch((err)=>console.log(err));
+}
